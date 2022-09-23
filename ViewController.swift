5d38d21f5b831a768f735da9feb8f01986854469
Reinaldo-Romagnoli <97524550+Reinaldo-Romagnoli@users.noreply.github.com>
2022-09-22 21:59:37 -0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon_nnT
+//
+//  Created by user on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+		
+    
+    @IBOutlet weak var Id_pokemon: UILabel!
+    @IBOutlet weak var Image_Pokemon: UIImageView!
+    @IBOutlet weak var Name_pokemon: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+}
+
+    @IBAction func Butto_action(_ sender: Any) {
+    }
+    
+}
