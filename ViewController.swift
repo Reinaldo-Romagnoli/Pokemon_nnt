47d8c31c5e5550fa7d461f3b00918c4e5ba72790
Reinaldo-Romagnoli <97524550+Reinaldo-Romagnoli@users.noreply.github.com>
2022-09-29 19:36:24 -0300

    ConsumoAPI
    Fazendo um consumo da api pokemon

--- ViewController.swift
+++ ViewController.swift
@@ -1,25 +1,93 @@
-//
-//  ViewController.swift
-//  Pokemon_nnT
-//
-//  Created by user on 22/09/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 		
+   
+    @IBOutlet weak var idpokemon: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var buttonStepper: UIStepper!
     
-    @IBOutlet weak var Id_pokemon: UILabel!
-    @IBOutlet weak var Image_Pokemon: UIImageView!
-    @IBOutlet weak var Name_pokemon: UILabel!
+   private var pokemonId = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
-}
-
-    @IBAction func Butto_action(_ sender: Any) {
+        buttonStepper.value = 1
+        buttonStepper.minimumValue = 1
+        buttonStepper.maximumValue = 151
+        
+        pokemonId = Int(buttonStepper.value)
+        idpokemon.text = "Pokemon ID: \(pokemonId)"
+        
+        makeRequest()
     }
     
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        pokemonId = Int(sender.value)
+        idpokemon.text = "Pokemon ID: \(pokemonId)"
+        
+        makeRequest()
+    }
+    
+    
+    private func makeRequest(){
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonId)/") else{
+            print(">>> URL passada como parametro é invalido")
+            return
+        }
+        
+        print(">>> Sucesso, URL passada é valida")
+        let urlRequest = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: urlRequest) {data, _, error in
+            if error != nil {
+                print(">>> Requisição Realizada, mas a resposta foi um Error")
+                return
+            }
+            
+            guard let data = data else {
+                print(">>>Error,data retornando vazio")
+                return
+            }
+            
+            if let response = try? JSONDecoder().decode(Response.self, from: data){
+                DispatchQueue.main.async {
+                    self.name.text = response.name.uppercased()
+                }
+                self.dowloadImage(url: response.sprites.other.official.urlImage)
+            } else {
+                print(">>> Error, a decodificação falhou verifique sua struct")
+            }
+        }.resume()
+        
+    }
+    
+    private func dowloadImage(url: String){
+        guard let url = URL(string: url) else{
+            print(">>> URL passada como parametro é invalido")
+            return
+        }
+        
+        print(">>> Sucesso, URL passada é valida")
+        let urlRequest = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: urlRequest) {data, _, error in
+            if error != nil {
+                print(">>> Requisição Realizada, mas a resposta foi um Error")
+                return
+            }
+            
+            guard let data = data else {
+                print(">>>Error,data retornando vazio")
+                return
+            }
+            
+            let uiImage = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.image.image = uiImage
+            }
+        }.resume()
+        
+    }
 }
